7c85a9f3a803fc029d3533802c91d70772f2b003
kvello <markus.kv1@gmail.com>
2023-11-03 20:55:22 +0800

    Add asynchronous message receiving and connection timeout feature to UDP server

--- src/server/udp_server.h
+++ src/server/udp_server.h
@@ -5,7 +5,28 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/time.h>
+#include <pthread.h>
 #include "../udp.h"
 #include "../msg.h"
 #include "../utils.h"
+
+static void* async_receive_message(void* args);
+
+typedef struct{
+    int socket;
+    struct StopAndWaitMessage* recv;
+    struct sockaddr* src_addr;
+    socklen_t addr_len;
+} rx_args;
+
+int init_server(struct StopAndWaitMessage* recv, 
+    struct StopAndWaitMessage* ack, 
+    int* socket, 
+    struct sockaddr_in* client_addr, 
+    socklen_t* addr_size, 
+    FILE* fp);
+
+static void clean_thread(void* args);
+
+#define CONNECTION_CLOSE_TIMEOUT 3000000 // 3 second
 #endif
--- src/utils.h
+++ src/utils.h
@@ -10,6 +10,7 @@
 #define NUM_BITS_CRC_DIVISOR 33
 #define NUM_BYTES_CRC_DIVISOR 5
 #define CRC_DIVISOR 0b100000100110000010001110110110111
+
 FILE* utils_open_file(char* file_name, const char* mode);
 bool utils_is_little_edian();
 int utils_findMSBPosition(uint8_t *x, int length, bool little_edian);
