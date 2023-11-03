4301c40a975c17fff039cf73db0678728b9b5040
kvello <markus.kv1@gmail.com>
2023-11-03 23:21:13 +0800

    Refactor udp_server.h to move unused struct and add clean_thread function

--- src/server/udp_server.h
+++ src/server/udp_server.h
@@ -17,12 +17,8 @@
 
 static void* async_receive_message(void* args);
 
-typedef struct{
-    int socket;
-    struct StopAndWaitMessage* recv;
-    struct sockaddr* src_addr;
-    socklen_t addr_len;
-} rx_args;
+static void clean_thread(void* args);
+
 int init_server(struct StopAndWaitMessage* recv, 
     struct StopAndWaitMessage* ack, 
     int socket, 
