d2255ec828718f4196b3f2c63f268feca5c2de09
kvello <markus.kv1@gmail.com>
2023-11-03 12:33:11 +0800

    Remove unused include and define in udp.h

--- src/udp.h
+++ src/udp.h
@@ -1,10 +1,8 @@
 #ifndef UDP_H
 #define UDP_H
 #include "common.h"
-#include "message.h"
 #define UDP_SERVER_PORT 5350
 #define UDP_CLIENT_PORT 5351
-#define UDP_DATA_SIZE MESSAGE_HEADER_SIZE+MESSAGE_DATA_SIZE
 
 int udp_receive(void* buf, int buf_size, const int socketfd, const void* in_addr, const socklen_t* addr_len);
 int udp_transmitt(void* data, int data_size, int sockfd, const void* out_addr, int addr_len, int protocol_family, int ser_port);
