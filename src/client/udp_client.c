a91fb5e5cdb0eac921f361eb62385585fbf0ff6b
kvello <markus.kv1@gmail.com>
2023-11-03 13:25:53 +0800

    Add UDP client functionality to transmit file using Stop-and-Wait protocol

--- src/client/udp_client.c
+++ src/client/udp_client.c
@@ -1,14 +1,29 @@
 #include "udp_client.h"
 
-FILE* open_file(char* file_name){
-    FILE *fp;
-    if((fp = fopen(file_name,"r"))==NULL){
-        printf("error in open file");
-        exit(1);
-    }
-    return fp;
-}
 
 int main(int argc,char** argv){
-    
+    int sock = udp_get_sock(AF_INET, UDP_CLIENT_PORT, INADDR_ANY);
+    struct sockaddr_in server_addr;
+    udp_get_sockaddr(AF_INET,argv[1],UDP_SERVER_PORT,&server_addr);
+    int fp = open_file(argv[2]);
+    uint8_t buf[MSG_MAX_DATA_SIZE];
+    struct StopAndWaitMessage out_message;
+    struct StopAndWaitMessage ack_message;
+    ack_message.header = msg_create_header(0,0,0,0);
+    out_message.header = msg_create_header(1,0,0,0);
+    int read_bytes=0;
+    while((read_bytes=read(fp,buf,MSG_MAX_DATA_SIZE))!=EOF){
+        int crc = utils_calculate_32crc(CRC_DIVISOR,buf,read_bytes);
+        struct StopAndWaitHeader header = msg_create_header(!out_message.header.seq_num,0,read_bytes,crc);
+        out_message = msg_create_message(header,buf);
+        while(ack_message.header.ack == out_message.header.seq_num){
+            int ret = msg_send_message(sock,&out_message, (struct sockaddr*)&server_addr);
+            if(ret == -1){
+                printf("error in udp_transmitt\n");
+                exit(1);
+            }
+            msg_receive_message(sock,&ack_message,(struct sockaddr* )NULL,sizeof(server_addr));
+        }
+
+    }
 }
--- src/client/udp_client.h
+++ src/client/udp_client.h
@@ -2,7 +2,8 @@
 #define UDP_CLIENT_H
 #include <stdio.h>
 #include <stdlib.h>
-#include "../common.h"
 #include <stdbool.h>
-#include "../message.h"
+#include "../msg.h"
+#include "../utils.h"
+#include "../udp.h"
 #endif
